39fa5061795c64b6b1f424aaa512743adc09042c
sss26 <s.sholto.smith@gmail.com>
2023-06-09 06:14:44 -0700

    Created bare-bones spring-boot version of myfancypdfinvoices

---
+++ src/test/java/com/smthasa/myfancypdfinvoices/springboot/MyfancypdfinvoicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smthasa.myfancypdfinvoices.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancypdfinvoicesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
